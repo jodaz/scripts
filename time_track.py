60bfba90f0a1ba3c6246f8fe7cce2fdbfc09d149
Jesus Ordosgoitty <jesuodz@gmail.com>
2018-06-24 20:58:47 -0400

    Add exception for new insertions

--- time_track.py
+++ time_track.py
@@ -5,26 +5,33 @@ client = MongoClient('localhost', 27017)
 db = client.timedata
 improvement_hours = db.improvement_hours
 
+def last_doc():
+    return improvement_hours.find_one()
+
 def ask_data():
     print("Add hours with format Work-College-Practice")
     data = input('>: ')
     return list(data)
 
 def main():
-    data = ask_data()
     current_date = datetime.date.today().strftime("%Y-%m-%d")
+    
+    if last_doc()["date"] == current_date:
+        print("Today's data already introduced.")
+    else:
+        data = ask_data()
 
-    document = {
-        'date': current_date,
-        'improvement_hours': [
-            {
-                'work_hours': data[0],
-                'college_hours': data[1],
-                'practice_hours': data[2]
-            }
-        ]
-    }
+        document = {
+            'date': current_date,
+            'improvement_hours': [
+                {
+                    'work_hours': data[0],
+                    'college_hours': data[1],
+                    'practice_hours': data[2]
+                }
+            ]
+        }
 
-    improvement_hours.insert_one(document)
+        improvement_hours.insert_one(document)
 
 main()
